67a38dc556ef5ef8417d298e44e6e1325c28c6a3
Son Le <sonlt@dgroup.co>
2021-06-12 21:53:08 +0700

    add prompt to unpack script

--- pack.sh
+++ pack.sh
@@ -1,3 +1,5 @@
 cp ~/.zshrc ./config
 cp ~/.tmux.conf ./config
 cp ~/.p10k.zsh ./config
+cp ~/.config/alacritty/alacritty.yml ./config
+cp -r ~/.config/karabiner ./config
--- unpack.sh
+++ unpack.sh
@@ -1,3 +1,10 @@
-cp ./config ~/.zshrc 
-cp ./config ~/.tmux.conf
-cp ./config ~/.p10k.zsh
+read -p "This will unpack dev config to this machine. Are you sure? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  cp ./config/.zshrc  ~/.zshrc 
+  cp ./config/.tmux.conf ~/.tmux.conf
+  cp ./config/.p10k.zsh ~/.p10k.zsh
+  cp ./config/alacritty.yml ~/.config/alacritty/alacritty.yml
+  cp -r ./config/karabiner ~/.config
+fi
